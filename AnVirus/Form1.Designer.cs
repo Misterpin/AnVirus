e4015eea7abbbb489cbb9cc21d80f36284ec663d
Misterpin <33574809+Misterpin@users.noreply.github.com>
2019-05-04 17:48:05 +0300

    Uploaded project

---
+++ AnVirus/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.clmH = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmH_ = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.kryptonButton1 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.kryptonButton2 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kryptonButton3 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmH,
+            this.clmH_,
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.Location = new System.Drawing.Point(23, 63);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(832, 682);
+            this.listView1.TabIndex = 24;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // clmH
+            // 
+            this.clmH.Text = "Антивирус";
+            this.clmH.Width = 208;
+            // 
+            // clmH_
+            // 
+            this.clmH_.Text = "Вирусы";
+            this.clmH_.Width = 208;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.Location = new System.Drawing.Point(6, 22);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(196, 25);
+            this.metroLabel1.TabIndex = 25;
+            this.metroLabel1.Text = "Введите путь до файла:";
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.Location = new System.Drawing.Point(6, 64);
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.Size = new System.Drawing.Size(328, 23);
+            this.metroTextBox1.TabIndex = 26;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(340, 64);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1.TabIndex = 27;
+            this.metroButton1.Text = "Обзор";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Дата сканирования";
+            this.columnHeader1.Width = 208;
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.Location = new System.Drawing.Point(176, 111);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.Size = new System.Drawing.Size(90, 25);
+            this.kryptonButton1.TabIndex = 28;
+            this.kryptonButton1.Values.Text = "Сканировать!";
+            this.kryptonButton1.Click += new System.EventHandler(this.kryptonButton1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(870, 63);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(473, 528);
+            this.tabControl1.TabIndex = 29;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.kryptonButton1);
+            this.tabPage1.Controls.Add(this.metroButton1);
+            this.tabPage1.Controls.Add(this.metroTextBox1);
+            this.tabPage1.Controls.Add(this.metroLabel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(465, 502);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Сканировать файл";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.kryptonButton3);
+            this.tabPage2.Controls.Add(this.kryptonButton2);
+            this.tabPage2.Controls.Add(this.metroTextBox2);
+            this.tabPage2.Controls.Add(this.metroLabel4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(465, 502);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Сканировать строку";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel2.Location = new System.Drawing.Point(880, 634);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(67, 25);
+            this.metroLabel2.TabIndex = 30;
+            this.metroLabel2.Text = "Статус:";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.CustomForeColor = true;
+            this.metroLabel3.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel3.Location = new System.Drawing.Point(953, 634);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(113, 25);
+            this.metroLabel3.Style = MetroFramework.MetroColorStyle.Orange;
+            this.metroLabel3.TabIndex = 31;
+            this.metroLabel3.Text = "остановлено";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel4.Location = new System.Drawing.Point(6, 26);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(135, 25);
+            this.metroLabel4.TabIndex = 26;
+            this.metroLabel4.Text = "Введите строку:";
+            // 
+            // metroTextBox2
+            // 
+            this.metroTextBox2.Location = new System.Drawing.Point(6, 75);
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.Size = new System.Drawing.Size(453, 23);
+            this.metroTextBox2.TabIndex = 27;
+            this.metroTextBox2.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // kryptonButton2
+            // 
+            this.kryptonButton2.Location = new System.Drawing.Point(102, 131);
+            this.kryptonButton2.Name = "kryptonButton2";
+            this.kryptonButton2.Size = new System.Drawing.Size(90, 25);
+            this.kryptonButton2.TabIndex = 28;
+            this.kryptonButton2.Values.Text = "Сканировать!";
+            this.kryptonButton2.Click += new System.EventHandler(this.kryptonButton2_Click);
+            // 
+            // kryptonButton3
+            // 
+            this.kryptonButton3.Location = new System.Drawing.Point(257, 131);
+            this.kryptonButton3.Name = "kryptonButton3";
+            this.kryptonButton3.Size = new System.Drawing.Size(105, 25);
+            this.kryptonButton3.TabIndex = 29;
+            this.kryptonButton3.Values.Text = "Тестовый вирус";
+            this.kryptonButton3.Click += new System.EventHandler(this.kryptonButton3_Click);
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Угроза";
+            this.columnHeader2.Width = 208;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1366, 768);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.listView1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Text = "Сканер";
+            this.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader clmH;
+        private System.Windows.Forms.ColumnHeader clmH_;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton3;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ AnVirus/Form1.cs
@@ -0,0 +1,254 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VirusTotalNET;
+using VirusTotalNET.ResponseCodes;
+using VirusTotalNET.Results;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void DoLogicString(string text)
+        {
+            listView1.Items.Clear();
+            byte[] virus = Encoding.ASCII.GetBytes(text);
+            FileReport fr = null;
+            try
+            {
+                fr = await ScanBytesAsync(virus);
+            }
+            catch (Exception e)
+            {
+                metroLabel3.Text = "Лимит скана - 4 раза в минуту!";
+                Console.Beep();
+                return;
+            }
+            ReportStructure structure = null;
+            if (fr != null)
+            {
+                structure = ReportStructure.FromReport(fr);
+            }
+            if (structure != null)
+            {
+                int i = 0;
+                foreach (KeyValuePair<string, bool> pair in structure.ScanResults)
+                {
+                    ListViewItem item = new ListViewItem(pair.Key);
+                    item.SubItems.Add(pair.Value ? "Имеются" : "Нет вирусов");
+                    item.SubItems.Add(structure.ScanDate.ToString());
+                    item.SubItems.Add(structure.VirNames[i]);
+                    listView1.Items.Add(item);
+                    i++;
+                }
+                int YesCount = 0;
+                int NoCount = 0;
+                foreach(bool vir in structure.ScanResults.Values)
+                {
+                    if(vir)
+                    {
+                        YesCount++;
+                    } else
+                    {
+                        NoCount++;
+                    }
+                }
+                if(YesCount > NoCount)
+                {
+                    metroLabel3.ForeColor = Color.Red;
+                    metroLabel3.Text = "вирус!";
+                    Console.Beep();
+                }
+            }
+        }
+
+        private async void DoLogicAsync(string path)
+        {
+            listView1.Items.Clear();
+            FileReport fr = null;
+            try
+            {
+                fr = await ScanFileAsync(path);
+            } catch(Exception e)
+            {
+                metroLabel3.Text = "Лимит скана - 4 раза в минуту!";
+                Console.Beep();
+                return;
+            }
+            ReportStructure structure = null;
+            if (fr != null) {
+                structure = ReportStructure.FromReport(fr);
+            }
+            if (structure != null) {
+                int i = 0;
+                foreach(KeyValuePair<string, bool> pair in structure.ScanResults)
+                {
+                    ListViewItem item = new ListViewItem(pair.Key);
+                    item.SubItems.Add(pair.Value ? "Да" : "Нет");
+                    item.SubItems.Add(structure.ScanDate.ToString());
+                    item.SubItems.Add(structure.VirNames[i]);
+                    listView1.Items.Add(item);
+                    i++;
+                }
+                int YesCount = 0;
+                int NoCount = 0;
+                foreach (bool vir in structure.ScanResults.Values)
+                {
+                    if (vir)
+                    {
+                        YesCount++;
+                    }
+                    else
+                    {
+                        NoCount++;
+                    }
+                }
+                if (YesCount > NoCount)
+                {
+                    metroLabel3.ForeColor = Color.Red;
+                    metroLabel3.Text = "вирус!";
+                    Console.Beep();
+                }
+            }
+            
+        }
+
+        private async Task<FileReport> ScanBytesAsync(byte[] bytes)
+        {
+            VirusTotal virusTotal = new VirusTotal("571adb9ec9c3d0614f1cf16ef8da0429b901eca6aeed8c84653b0e7f6ddf5da4");
+            virusTotal.UseTLS = true;
+            FileReport report = await virusTotal.GetFileReportAsync(bytes);
+            bool hasFileBeenScannedBefore = report.ResponseCode == FileReportResponseCode.Present;
+            if (hasFileBeenScannedBefore)
+            {
+                metroLabel3.ForeColor = Color.Green;
+                metroLabel3.Text = "успешно";
+                return report;
+            }
+            else
+            {
+                ScanResult fileResult = await virusTotal.ScanFileAsync(bytes, "Eicar.txt");
+                metroLabel3.ForeColor = Color.Green;
+                metroLabel3.Text = "идет сканирование. Подождите приблизительно 2 минуты и сканируйте снова!";
+                report = null;
+                return report;
+            }
+        }
+
+        private async Task<FileReport> ScanFileAsync(string path)
+        {
+            FileInfo info = new FileInfo(path);
+            VirusTotal vt = new VirusTotal("571adb9ec9c3d0614f1cf16ef8da0429b901eca6aeed8c84653b0e7f6ddf5da4");
+            vt.UseTLS = true;
+            FileReport fileReport = await vt.GetFileReportAsync(info);
+            bool hasFileBeenScannedBefore = fileReport.ResponseCode == FileReportResponseCode.Present;
+            if (!hasFileBeenScannedBefore)
+            {
+                ScanResult fileResult = await vt.ScanFileAsync(info);
+                metroLabel3.ForeColor = Color.Green;
+                metroLabel3.Text = "идет сканирование. Подождите приблизительно 2 минуты и сканируйте снова!";
+                return null;
+            }
+            else
+            {
+                metroLabel3.ForeColor = Color.Green;
+                metroLabel3.Text = "успешно";
+                return fileReport;
+            }
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                metroTextBox1.Text = path;
+            }
+        }
+
+        private void kryptonButton1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(metroTextBox1.Text) || string.IsNullOrEmpty(metroTextBox1.Text))
+            {
+                metroLabel3.ForeColor = Color.Red;
+                metroLabel3.Text = "путь файла не может быть пустым!";
+                return;
+            }
+            if(!File.Exists(metroTextBox1.Text))
+            {
+                metroLabel3.ForeColor = Color.Red;
+                metroLabel3.Text = "указанный файл не существует!";
+                return;
+            }
+            if (!CheckInternetConnection())
+            {
+                metroLabel3.ForeColor = Color.Red;
+                metroLabel3.Text = "нет соединения!";
+                Console.Beep();
+                return;
+            }
+            DoLogicAsync(metroTextBox1.Text);
+        }
+
+        private void kryptonButton2_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(metroTextBox2.Text) || string.IsNullOrEmpty(metroTextBox2.Text))
+            {
+                metroLabel3.ForeColor = Color.Red;
+                metroLabel3.Text = "строка не может быть пустой!";
+                return;
+            }
+            if(!CheckInternetConnection())
+            {
+                metroLabel3.ForeColor = Color.Red;
+                metroLabel3.Text = "нет соединения!";
+                Console.Beep();
+                return;
+            }
+            DoLogicString(metroTextBox2.Text);
+        }
+
+        private void kryptonButton3_Click(object sender, EventArgs e)
+        {
+            metroTextBox2.Text = @"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*";
+        }
+
+        private bool CheckInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (var stream = client.OpenRead("http://www.google.com"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
